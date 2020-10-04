64b74beabaa23d0497ee633ff0ed3b80d3851163
Kleory <okleory@gmail.com>
2020-10-04 13:48:41 +0300

    feat: implement what-season

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(/*arr*/) {
+    throw new CustomError("Not implemented");
     // remove line with error and write your code here
+
+    // let length = 1;
+    // let count = [];
+    // function deep(item) {
+    //   if (item.isArray) {
+    //     item.map((itemN) => {
+    //       if (itemN.isArray()) {
+    //         length += 1;
+    //         deep(itemN);
+    //       } else {
+    //         count.push(length);
+    //         length -= 1;
+    //       }
+    //     });
+    //   }
+    // }
+
+    // arr.map((item) => deep(item));
+    // console.log(count);
+    // let max = Math.max(...count);
+    // return max;
   }
 };

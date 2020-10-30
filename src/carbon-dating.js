ded594aeeac418884054d9facd5b112b4df796e4
Kleory <okleory@gmail.com>
2020-10-30 19:42:37 +0300

    feat: implement task carbon-dating and transform-array

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,13 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
- 
-
-  return false;
+  let t
+  if (typeof sampleActivity ==='string' && !typeof +sampleActivity === 'number' && +sampleActivity > 0 && +sampleActivity <= 15){
+    t = Math.ceil((Math.log(15/sampleActivity)) / (Math.log(2) / 5730)); 
+    return t;
+    
+  }else {
+    return false;
+    };
+  
 };
